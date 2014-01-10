719658c43ac03e70fd588485b9a69c986d7fc4e0
Bruno <bmgit@bmconseil.com>
2014-01-10 15:17:15 +0100

    added css methods and CDN urls

--- lib/jquery-ui-rails-cdn.rb
+++ lib/jquery-ui-rails-cdn.rb
@@ -14,6 +14,13 @@ module Jquery::Ui::Rails::Cdn
       :cloudflare => "//cdnjs.cloudflare.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.min.js"
     }
 
+    CSS_URL = {
+      :google     => "//ajax.googleapis.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/themes/THEME_PLACEHOLDER/jquery-ui.min.css",
+      :microsoft  => "//ajax.aspnetcdn.com/ajax/jquery.ui/#{JQUERY_UI_VERSION}/themes/THEME_PLACEHOLDER/jquery-ui.min.css",
+      :jquery     => "//code.jquery.com/ui/#{JQUERY_UI_VERSION}/themes/THEME_PLACEHOLDER/jquery-ui.min.css",
+      :yandex     => "//yandex.st/jquery-ui/#{JQUERY_UI_VERSION}/themes/THEME_PLACEHOLDER/jquery-ui.min.css",
+    }
+
     def jquery_ui_javascript_url(name, options = {})
       return JS_URL[name]
     end
@@ -28,6 +35,22 @@ module Jquery::Ui::Rails::Cdn
 
     alias_method :jquery_ui_url, :jquery_ui_javascript_url
     alias_method :jquery_ui_include_tag, :jquery_ui_javascript_tag
+
+    def jquery_ui_stylesheet_url(name, options = {})
+      # Fallback theme is 'smoothness'
+      theme = options[:theme] || 'smoothness'
+
+      # Build CSS URL
+      return CSS_URL[name].to_s.gsub('THEME_PLACEHOLDER',theme)
+    end
+
+    def jquery_ui_stylesheet_tag(name, options = {})
+      # Return tag with local reference if offline or forced
+      return stylesheet_link_tag(:'jquery.ui.all') if OFFLINE and !options[:force]
+
+      # Build CSS tag
+      stylesheet_link_tag jquery_ui_stylesheet_url(name, options)
+    end
 
   end
 
