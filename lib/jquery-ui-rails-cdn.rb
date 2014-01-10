a0834dc7c6f0758c69482baae52b1daea0c378b7
Bruno <bmgit@bmconseil.com>
2014-01-10 15:17:41 +0100

    query CDN does provide HTTP and HTTPS access, protocol spec removed

--- lib/jquery-ui-rails-cdn.rb
+++ lib/jquery-ui-rails-cdn.rb
@@ -9,7 +9,8 @@ module Jquery::Ui::Rails::Cdn
     JS_URL = {
       :google     => "//ajax.googleapis.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
       :microsoft  => "//ajax.aspnetcdn.com/ajax/jquery.ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
-      :jquery     => "http://code.jquery.com/ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
+      :jquery     => "//code.jquery.com/ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
+
       :yandex     => "//yandex.st/jquery-ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
       :cloudflare => "//cdnjs.cloudflare.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.min.js"
     }
