d22bb992a0c5f85215c7880dd1d659471ffa8475
Bruno <bmgit@bmconseil.com>
2014-01-10 14:39:14 +0100

    renamed JS constants and methods to introduce CSS methods, adding aliases for backward-compatibility

--- lib/jquery-ui-rails-cdn.rb
+++ lib/jquery-ui-rails-cdn.rb
@@ -6,7 +6,7 @@ module Jquery::Ui::Rails::Cdn
     JQUERY_UI_VERSION = Jquery::Ui::Rails::JQUERY_UI_VERSION
     OFFLINE = (Rails.env.development? or Rails.env.test?)
 
-    URL = {
+    JS_URL = {
       :google     => "//ajax.googleapis.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
       :microsoft  => "//ajax.aspnetcdn.com/ajax/jquery.ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
       :jquery     => "http://code.jquery.com/ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
@@ -14,17 +14,21 @@ module Jquery::Ui::Rails::Cdn
       :cloudflare => "//cdnjs.cloudflare.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.min.js"
     }
 
-    def jquery_ui_url(name, options = {})
-      return URL[name]
+    def jquery_ui_javascript_url(name, options = {})
+      return JS_URL[name]
     end
 
-    def jquery_ui_include_tag(name, options = {})
+    def jquery_ui_javascript_tag(name, options = {})
       return javascript_include_tag(:'jquery.ui.all') if OFFLINE and !options[:force]
 
-      [ javascript_include_tag(jquery_ui_url(name, options)),
+      [ javascript_include_tag(jquery_ui_javascript_url(name, options)),
         javascript_tag("window.jQuery.ui || document.write(unescape('#{javascript_include_tag(:'jquery.ui.all').gsub('<','%3C')}'))")
       ].join("\n").html_safe
     end
+
+    alias_method :jquery_ui_url, :jquery_ui_javascript_url
+    alias_method :jquery_ui_include_tag, :jquery_ui_javascript_tag
+
   end
 
   class Railtie < Rails::Railtie
